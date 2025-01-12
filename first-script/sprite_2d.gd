a052ee3dfbb9ec51dc7963f09b3964274f066a89
Ethan Davis <121524320+ethandav@users.noreply.github.com>
2025-01-11 21:22:35 -0800

    signals

--- first-script/sprite_2d.gd
+++ first-script/sprite_2d.gd
@@ -4,15 +4,22 @@ var speed = 400
 var angular_speed = PI
 
 func _process(delta):
-	var velocity = Vector2.ZERO
+	#var velocity = Vector2.ZERO
 
-	var direction = 0
-	if Input.is_action_pressed("ui_left"):
-		direction = -1
-	if Input.is_action_pressed("ui_right"):
-		direction = 1
-	if Input.is_action_pressed("ui_up"):
-		velocity = Vector2.UP.rotated(rotation) * speed
-	rotation += angular_speed * direction * delta
+	#var direction = 0
+	#if Input.is_action_pressed("ui_left"):
+	#	direction = -1
+	#if Input.is_action_pressed("ui_right"):
+	#	direction = 1
+	#if Input.is_action_pressed("ui_up"):
+	#	velocity = Vector2.UP.rotated(rotation) * speed
+	#rotation += angular_speed * direction * delta
+	#position += velocity * delta
+	rotation += angular_speed * delta
+	var velocity = Vector2.UP.rotated(rotation) * speed
 	position += velocity * delta
 	
+
+
+func _on_button_pressed() -> void:
+	set_process(not is_processing())
