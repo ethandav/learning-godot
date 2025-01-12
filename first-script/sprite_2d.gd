838f6a0f6311ef3fbf3ea3eb808d5b5d1d70b287
Ethan Davis <121524320+ethandav@users.noreply.github.com>
2025-01-11 21:30:28 -0800

    signals by code

--- first-script/sprite_2d.gd
+++ first-script/sprite_2d.gd
@@ -3,6 +3,10 @@ extends Sprite2D
 var speed = 400
 var angular_speed = PI
 
+func _ready():
+	var timer = get_node("Timer")
+	timer.timeout.connect(_on_timer_timeout)
+
 func _process(delta):
 	#var velocity = Vector2.ZERO
 
@@ -20,6 +24,8 @@ func _process(delta):
 	position += velocity * delta
 	
 
-
 func _on_button_pressed() -> void:
 	set_process(not is_processing())
+	
+func _on_timer_timeout():
+	visible = not visible
