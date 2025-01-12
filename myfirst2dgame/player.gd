8aa2bd00d40e0a37fcc64d92121615d3b2222914
Ethan Davis <121524320+ethandav@users.noreply.github.com>
2025-01-12 00:46:12 -0800

    collision setup

--- myfirst2dgame/player.gd
+++ myfirst2dgame/player.gd
@@ -1,10 +1,13 @@
 extends Area2D
 
+signal hit
+
 @export var speed = 400
 var screen_size
 
 func _ready():
 	screen_size = get_viewport_rect().size
+	hide()
 	
 func _process(delta):
 	var velocity = Vector2.ZERO
@@ -33,3 +36,16 @@ func _process(delta):
 	elif velocity.y != 0:
 		$AnimatedSprite2D.animation = "up"
 		$AnimatedSprite2D.flip_v = velocity.y > 0
+
+
+func _on_body_entered(body: Node2D) -> void:
+	hide()
+	hit.emit()
+	$CollisionShape2D.set_deferred("disabled", true)
+	
+func start(pos):
+		position = pos
+		show()
+		$CollisionShape2D.disabled = false
+	
+	
