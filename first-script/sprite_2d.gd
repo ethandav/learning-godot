8789ea59bb22e13df17a26bc248a5a901fb1271d
Ethan Davis <121524320+ethandav@users.noreply.github.com>
2025-01-11 21:01:03 -0800

    player movement rotate

--- first-script/sprite_2d.gd
+++ first-script/sprite_2d.gd
@@ -4,7 +4,13 @@ var speed = 400
 var angular_speed = PI
 
 func _process(delta):
-	rotation += angular_speed * delta
-	var velocity = Vector2.UP.rotated(rotation) * speed
+	#rotation += angular_speed * delta
+	#var velocity = Vector2.UP.rotated(rotation) * speed
 
-	position += velocity * delta
+	#position += velocity * delta
+	var direction = 0
+	if Input.is_action_pressed("ui_left"):
+		direction = -1
+	if Input.is_action_pressed("ui_right"):
+		direction = 1 
+	rotation += angular_speed * direction * delta
